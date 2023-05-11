5fa2657a4cd59a4225ec6b5b17c08bbfabe6a5d6
stevemao <steve.mao@stonybrook.edu>
2023-05-11 21:38:13 +0000

    trying to push to vercel part by part

--- main.py
+++ main.py
@@ -9,24 +9,4 @@ app = FastAPI()
 @app.get("/")
 def home(): 
     return {"this is a API service for MN drug summary details"}
-
-@app.get("/preview")
-def preview(): 
-    top10rows = df.head(1)
-    result = top10rows.to_json(orient="records")
-    return result
-
-@app.get("/service/{value}")
-def service(value): 
-    print("value: ", value)
-    filtered = df[df["svc_code"] == value]
-    if len(filtered) <= 0:
-        return {"There is nothing here"}
-    else:
-        return filtered.to_json(orient="records")
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
+    
